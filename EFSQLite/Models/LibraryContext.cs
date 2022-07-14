fa9a1e0aa5ab2ffc22446407865439551af23589
jjvera96 <dev.jjvera96@gmail.com>
2022-07-14 10:58:35 -0500

    Add project files.

---
+++ EFSQLite/Models/LibraryContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EFSQLite.Models
+{
+    internal class LibraryContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            /*Assembly? assembly = Assembly.GetEntryAssembly();
+            ArgumentNullException.ThrowIfNull(assembly);
+            string? directoryName = Path.GetDirectoryName(assembly.Location);
+            ArgumentNullException.ThrowIfNull(directoryName);
+            */
+            optionsBuilder.UseSqlite($"Data Source={Path.Combine("path", "database.db")}");
+        }
+    }
+}
---
+++ EFSQLite/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EFSQLite.Models;
+
+using var context = new LibraryContext();
+/*
+context.Database.EnsureCreated();
+*/
+/*
+var author = new Author { Name = "Juan", LastName = "Vera" };
+var book = new Book { Title = "Viviendo", Sinopsis = "Libro sobre como vivir", Author = author };
+author.Books.Add(book);
+context.Authors.Add(author);
+context.Books.Add(book);
+await context.SaveChangesAsync();
+*/
+var bookFromDb = context.Books.FirstOrDefault(b => b.Title == "Viviendo");
+var authorFromDb = context.Authors.FirstOrDefault(b => b.Name == "Juan");
+ArgumentNullException.ThrowIfNull(bookFromDb);
+ArgumentNullException.ThrowIfNull(authorFromDb);
+Console.WriteLine(bookFromDb.BookId);
+Console.WriteLine(authorFromDb.Books.First().BookId);
